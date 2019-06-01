c3f006941a95cf8d752bbd1bb17f3046baaf1b37
MitchellStride <mitchellcstride@gmail.com>
2019-06-01 02:14:08 -0230

    Adding code template and pdf sch

---
+++ ControllerPCB/CODE/2019_Controller/2019_Controller.ino
@@ -0,0 +1,25 @@
+/*******************************************
+   2019 Baja Uno Shield Controller
+ ******************************************/
+
+ // Libraries
+//MAX6675
+//DS1207+ RTC
+//NRF Library
+//SD Card
+//DS18b20
+
+// PIN ASSIGNMENTS
+#define panelLED 7
+//I2C
+//SPI Devices
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
